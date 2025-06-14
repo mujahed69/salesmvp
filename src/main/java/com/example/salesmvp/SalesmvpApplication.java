b983b197e9656b65957ef6989d88983c4550ddfd
mujahed69 <mujahed69@gmail.com>
2025-06-14 18:01:21 +0530

    basic code

---
+++ src/main/java/com/example/salesmvp/SalesmvpApplication.java
@@ -0,0 +1,13 @@
+package com.example.salesmvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesmvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesmvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/salesmvp/SalesmvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salesmvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesmvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
